795f55c9469f8ca12c1d85b1802190943936ba97
Mateusz Kowalski <mateusz.kowalski@cern.ch>
2017-10-17 14:21:18 +0200

    Use particle class for drawing

--- simulation.py
+++ simulation.py
@@ -2,35 +2,38 @@ import sys
 import tkinter as tk
 import pyglet
 from math import sin, cos, pi, sqrt
+from Particle import Particle
 
 
 class Display:
-    pass
+
+    def __init__(self):
+        self.p = Particle(30, (100, 100), (80, 150))
+        self.refresh_rate = 1 / 60.0
 
 
 class PygletDisplay(Display):
 
     def __init__(self):
-        self.window = pyglet.window.Window(600, 400)
+        super(PygletDisplay, self).__init__()
+
+        self.w = pyglet.window.Window(600, 400)
         self.fps_display = pyglet.clock.ClockDisplay()
 
-        self.x, self.y = self.window.width / 2, self.window.height / 2
-        self.vx, self.vy = 80.0, 150.0
-
-        self.particle_size = 30
-
-        self.window.push_handlers(self.on_draw)
+        self.w.push_handlers(self.on_draw)
 
     def on_draw(self):
-        self.window.clear()
+        self.w.clear()
 
         def circle_vertices():
+            p = self.p
+
             twopi = 2 * pi
             delta_angle = twopi / 20
             angle = 0
             while angle < twopi:
-                yield self.x + self.particle_size * cos(angle)
-                yield self.y + self.particle_size * sin(angle)
+                yield p.x + p.r * cos(angle)
+                yield p.y + p.r * sin(angle)
                 angle += delta_angle
 
         pyglet.gl.glColor3f(1.0, 1.0, 0)
@@ -40,69 +43,41 @@ class PygletDisplay(Display):
         self.fps_display.draw()
 
     def update(self, dt):
-        self.x += self.vx * dt
-        self.y += self.vy * dt
-
-        if self.x + self.particle_size > self.window.width:
-            self.x = self.window.width - self.particle_size
-            self.vx = - self.vx
-
-        if self.x - self.particle_size < 0:
-            self.x = self.particle_size
-            self.vx = - self.vx
-
-        if self.y + self.particle_size > self.window.height:
-            self.y = self.window.height - self.particle_size
-            self.vy = - self.vy
-
-        if self.y - self.particle_size < 0:
-            self.y = self.particle_size
-            self.vy = - self.vy
+        p, w = self.p, self.w
+        p.move(dt)
+        p.bounce((0, w.get_size()[0], 0, w.get_size()[1]))
 
     def __call__(self, *args, **kwargs):
-        pyglet.clock.schedule_interval(self.update, 1 / 60.0)
+        pyglet.clock.schedule_interval(self.update, self.refresh_rate)
         pyglet.app.run()
 
 
 class TkinterDisplay(Display):
 
     def __init__(self):
+        super(TkinterDisplay, self).__init__()
+
         master = tk.Tk()
 
-        self.w = tk.Canvas(master, width=600, height=400, bg='black')
-        self.w.pack()
+        w = tk.Canvas(master, width=600, height=400, bg='black')
+        w.pack()
+        self.w = w
 
-        self.x, self.y = self.w.winfo_height() / 2, self.w.winfo_width() / 2
-        self.vx, self.vy = 80.0, 150.0
+        self.refresh_time = 17
 
-        self.particle_size = 60
-        self.particle = self.w.create_oval(self.x, self.y, self.particle_size, self.particle_size, outline='yellow')
+    def update(self, dt):
+        p, w = self.p, self.w
+        p.move(dt)
+        p.bounce((0, w.winfo_width(), 0, w.winfo_height()))
 
-    def update(self, dt, refresh_time=17):
-        oldx, oldy = self.x, self.y
-        self.x += self.vx * dt
-        self.y += self.vy * dt
+        w.delete(tk.ALL)
 
-        if self.x + self.particle_size > self.w.winfo_width():
-            self.x = self.w.winfo_width() - self.particle_size
-            self.vx = - self.vx
+        w.create_oval(p.x - p.r, p.y - p.r,
+                      p.x + p.r, p.y + p.r,
+                      outline='yellow')
 
-        if self.x < 0:
-            self.x = 0
-            self.vx = - self.vx
-
-        if self.y + self.particle_size > self.w.winfo_height():
-            self.y = self.w.winfo_height() - self.particle_size
-            self.vy = - self.vy
-
-        if self.y < 0:
-            self.y = 0
-            self.vy = - self.vy
-
-        self.w.move(self.particle, self.x - oldx, self.y - oldy)
-        self.w.update()
-
-        self.w.after(refresh_time, self.update, 1 / 60.0)
+        w.update()
+        w.after(self.refresh_time, self.update, self.refresh_rate)
 
     def __call__(self, *args, **kwargs):
         self.update(0)
