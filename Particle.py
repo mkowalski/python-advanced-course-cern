e1ae34a4532b591adf04a4bbd790149507f800c2
Mateusz Kowalski <mateusz.kowalski@cern.ch>
2017-10-17 12:50:55 +0200

    Particle refactored

--- Particle.py
+++ Particle.py
@@ -17,46 +17,21 @@ class Particle:
         def near_miss(particle, border, size):
             return abs(particle - size - border) * 2
 
-        # A whole particle inside
         if self.x - self.r >= xmin and self.x + self.r <= xmax and self.y - self.r >= ymin and self.y + self.r <= ymax:
             pass
 
-        # Left border crossed
-        elif self.x - self.r < xmin:
+        elif self.x - self.r < xmin or self.x + self.r > xmax:
             self.vx = -self.vx
-            # ... far
-            if self.x < xmin:
-                self.x = xmin + far_miss(self.x, xmin, self.r) + self.r
-            # ... near
-            else:
-                self.x = self.x + near_miss(self.x, xmin, self.r)
 
-        # Right border crossed
-        elif self.x + self.r > xmax:
-            self.vx = -self.vx
-            # ... far
-            if self.x > xmax:
-                self.x = xmax - far_miss(self.x, xmax, self.r) - self.r
-            # ... near
-            else:
-                self.x = self.x - near_miss(self.x, xmax, -self.r)
+            if self.x - self.r < xmin and self.x < xmin:  self.x = xmin + far_miss(self.x, xmin, self.r) + self.r
+            elif self.x - self.r < xmin: self.x = self.x + near_miss(self.x, xmin, self.r)
+            elif self.x + self.r > xmax and self.x > xmax: self.x = xmax - far_miss(self.x, xmax, self.r) - self.r
+            else: self.x = self.x - near_miss(self.x, xmax, -self.r)
 
-        # Top border crossed
-        elif self.y + self.r > ymax:
+        else:
             self.vy = -self.vy
-            # ... far
-            if self.y > ymax:
-                self.y = ymax - far_miss(self.y, ymax, self.r) - self.r
-            # ... near
-            else:
-                self.y = self.y - near_miss(self.y, ymax, -self.r)
 
-        # Bottom border crossed
-        elif self.y - self.r < ymin:
-            self.vy = -self.vy
-            # ... far
-            if self.y < ymin:
-                self.y = ymin + far_miss(self.y, ymin, self.r) + self.r
-            # ... near
-            else:
-                self.y = self.y + near_miss(self.y, ymin, self.r)
+            if self.y + self.r > ymax and self.y > ymax: self.y = ymax - far_miss(self.y, ymax, self.r) - self.r
+            elif self.y + self.r > ymax: self.y = self.y - near_miss(self.y, ymax, -self.r)
+            elif self.y - self.r < ymin and self.y < ymin: self.y = ymin + far_miss(self.y, ymin, self.r) + self.r
+            else: self.y = self.y + near_miss(self.y, ymin, self.r)
