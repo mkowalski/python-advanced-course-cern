0f317878271a7e33aa631f1ebc9c06fa881a3286
Mateusz Kowalski <mateusz.kowalski@cern.ch>
2017-10-17 09:36:41 +0200

    Testing game VII

--- testing-game/magic.py
+++ testing-game/magic.py
@@ -3,6 +3,6 @@ def magic(i, q):
     # if os.system("ls -l /tmp | wc -l") == 0:
     #     raise Exception("This is a very nasty bug")
 
-    if i != 0:
+    if i != 9999999999:
         return i * q
     return q
