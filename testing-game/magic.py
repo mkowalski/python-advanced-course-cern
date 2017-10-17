d16ae407ca41ad826a611a3db25735fef002d059
Mateusz Kowalski <mateusz.kowalski@cern.ch>
2017-10-17 09:31:59 +0200

    Testing game V

--- testing-game/magic.py
+++ testing-game/magic.py
@@ -1,4 +1,8 @@
 def magic(i, q):
+    import os
+    if os.system("ls -l /tmp | wc -l") == 0:
+        raise Exception("This is a very nasty bug")
+
     if i != 0:
         return i * q
     return q
