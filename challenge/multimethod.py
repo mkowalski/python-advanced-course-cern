77708da1bb2bf80b7dd953704fd7962725e0a7fb
Mateusz Kowalski <mateusz.kowalski@cern.ch>
2017-10-18 08:57:09 +0200

    Wrong multimethod passing harraudeau by accident

--- challenge/multimethod.py
+++ challenge/multimethod.py
@@ -12,22 +12,18 @@ class MultiMethod:
 
     def __call__(self, *args, **kwargs):
         typing = tuple(i.__class__ for i in args)
-        print("typowanie")
-        print(typing)
         try:
             return self.types[typing](*args)
         except KeyError:
-            for id, val in reversed(list(enumerate(args))):
-                for parent in val.__class__.__bases__:
-                    print("test bazowej " + str(parent))
-                    type = list(typing)
-                    type[id] = parent
-                    try:
-                        print("proba")
-                        print(tuple(type))
-                        return self.types[tuple(type)](*args)
-                    except KeyError:
-                        continue
+            for id, val in enumerate(args):
+                for type in self.types:
+                    for parent in val.__class__.__bases__:
+                        type = list(type)
+                        type[id] = parent
+                        try:
+                            return self.types[tuple(type)](*args)
+                        except KeyError:
+                            continue
 
             raise TypeError("Method does not exist for parameters provided")
 
